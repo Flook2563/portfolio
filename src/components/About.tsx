d8d8a81b6f08442645438dcefa06bfb52ede3e00
Flook2563 <Flook2563@gmail.com>
2025-06-06 00:45:01 +0700

    Update Download

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -107,8 +107,9 @@ export const About = () => {
                 <span className="px-4 py-1 bg-secondary text-secondary-foreground rounded-full text-sm font-medium hover:scale-110 hover:shadow-lg transition-all duration-300 ease-in-out cursor-default">Backend</span>
               </div>
               <a
-                href="public\Resume.pdf" // Path to your resume file in the public folder
-                download="Nattanon_Hanpap_Resume.pdf" // Suggested filename for the download
+                href="https://drive.google.com/file/d/1rk9UPBBHQMgU9cUPcjz3cexRzBwP0H3H/view?usp=sharing"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="w-full bg-primary hover:bg-primary/90 text-primary-foreground font-semibold py-3 px-6 rounded-lg shadow-md flex items-center justify-center transition-colors duration-300"
               >
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-2 h-5 w-5 lucide lucide-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" x2="12" y1="15" y2="3"/></svg>
